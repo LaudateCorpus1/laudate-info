dd3156523b90404b76fa0ee99b0fc9f2b6c85d56
Guillaume CHOMAT <guillaume.chomat@up.coop>
2021-08-10 08:53:50 +0200

    Init project

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'e89b343cab99a355c20378b815ffc46b'),
+    },
+  },
+});
