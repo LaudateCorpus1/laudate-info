76be8bb938b71455eb2063adb3c35ff365f9e68b
Guillaume CHOMAT <guillaume.chomat@up.coop>
2021-08-17 22:16:07 +0200

    Fix auth

--- backend/config/env/production/server.js
+++ backend/config/env/production/server.js
@@ -1,3 +1,8 @@
 module.exports = ({ env }) => ({
     url: env('MY_HEROKU_URL'),
+    admin: {
+        auth: {
+          secret: env('ADMIN_JWT_SECRET', 'e89b343cab99a355c20378b815ffc46b'),
+        },
+    },
 });
